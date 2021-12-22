715f6b15a73f4ded8fc0b147736820f3a248ca27
Francisco Louro <francisco.louro@infosistema.com>
2021-12-22 16:15:09 +0000

    Fixed run script

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 if [[ "$(docker images -q emailfetcher:1.0 2> /dev/null)" == "" ]]; then
+  cd $HOME/EmailFetcher
   docker build -t emailfetcher:1.0 .
   docker run emailfetcher:1.0
 else
